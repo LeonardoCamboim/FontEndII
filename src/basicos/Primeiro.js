3f6178cc443fd0e18773c68feec9f3da577a2b09
Leonardo <almleon@gmail.com>
2023-09-17 15:15:35 -0300

    commit

--- src/basicos/Primeiro.js
+++ src/basicos/Primeiro.js
@@ -3,9 +3,14 @@ import React from 'react';
 function Primeiro() {
   return (
     <div>
-      <p>Este é o primeiro parágrafo.</p>
-      <p>Este é o segundo parágrafo.</p>
-      <p>Este é o terceiro parágrafo.</p>
+      <h1>Projeto Didático de Front End II</h1>
+      <p>Este é um projeto didático desenvolvido como parte do curso de Front End II.</p>
+      <ul>
+        <li>Desenvolvido por: Leonardo de Almeida Camboim</li>
+        <li>Disciplina: Front End II</li>
+        <li>Objetivo: Aplicar os conceitos e técnicas aprendidos durante o curso para criar uma aplicação da web simples.</li>
+      </ul>
+      <a href="https://github.com/LeonardoCamboim/FontEndII">Link para o projeto no GitHub</a>
     </div>
   );
 }
