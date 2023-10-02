970186bf86277372b6e1ecaa8f2ad627202c9d59
Leonardo <almleon@gmail.com>
2023-10-02 11:42:13 -0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import Primeiro from './basicos/Primeiro'; // Importe o componente Primeiro
-
-
+import NovoComponente from './basicos/NovoComponente';
 
 function App() {
   return (
     <div>
       <h1>Meu App React</h1>
-      <Primeiro />
+      <NovoComponente tituloH1="Disciplina: Front End II" tituloH2="Desenvolvido por: Leonardo de Almeida Camboim" />
     </div>
   );
 }
---
+++ src/basicos/NovoComponente.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function NovoComponente(props) {
+  return (
+    <div>
+      <h1>{props.tituloH1}</h1>
+      <h2>{props.tituloH2}</h2>
+    </div>
+  );
+}
+
+export default NovoComponente;
